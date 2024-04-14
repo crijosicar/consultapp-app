cf45c5f981fcc3a6218fa02ea9289e00abd31f89
Cristian Sierra <crijosicar@hotmail.com>
2024-04-14 15:25:58 -0400

    Fix lint rules

--- src/app/faqs/page.tsx
+++ src/app/faqs/page.tsx
@@ -6,9 +6,7 @@ export default function Faqs(): ReactElement {
     return (
         <div className="relative h-screen w-full lg:ps-64">
             <div className="py-10 lg:py-14">
-                {/* FAQ */}
                 <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
-                    {/* Grid */}
                     <div className={'m-20 flex flex-col'}>
                         <div className="grid md:grid-cols-5 gap-10">
                             <div className="md:col-span-2">
@@ -23,9 +21,7 @@ export default function Faqs(): ReactElement {
                                     </p>
                                 </div>
                             </div>
-                            {/* End Col */}
                             <div className="md:col-span-3">
-                                {/* Accordion */}
                                 <div className="hs-accordion-group divide-y divide-gray-200">
                                     <div
                                         className="hs-accordion pb-3 active"
@@ -145,7 +141,7 @@ export default function Faqs(): ReactElement {
                                                 you are traveling by air, you might consider applying for an Electronic
                                                 Travel Authorization (eTA) if you meet specific conditions, like having
                                                 held a Canadian visa in the past ten years or currently holding a valid
-                                                United States nonimmigrant visa.
+                                                United States non-immigrant visa.
                                             </p>
                                         </div>
                                     </div>
@@ -193,15 +189,15 @@ export default function Faqs(): ReactElement {
                                             aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-three"
                                         >
                                             <p className="text-gray-600">
-                                                The fee for a Canadian Visitor Visa (Temporary Resident Visa) is
-                                                typically CAD $100 per person. In addition to the application fee, you
-                                                might also have to pay for biometrics, which costs CAD $85. If you are
-                                                applying as a family, a maximum total fee for biometrics is CAD $170.
-
-                                                These fees are subject to change, so it's a good idea to check the
-                                                official Immigration, Refugees and Citizenship Canada (IRCC) website or
-                                                consult with the nearest Canadian embassy or consulate for the most
-                                                up-to-date information.
+                                                {"The fee for a Canadian Visitor Visa (Temporary Resident Visa) is\n" +
+                                                    "                                                typically CAD $100 per person. In addition to the application fee, you\n" +
+                                                    "                                                might also have to pay for biometrics, which costs CAD $85. If you are\n" +
+                                                    "                                                applying as a family, a maximum total fee for biometrics is CAD $170.\n" +
+                                                    "\n" +
+                                                    "                                                These fees are subject to change, so it's a good idea to check the\n" +
+                                                    "                                                official Immigration, Refugees and Citizenship Canada (IRCC) website or\n" +
+                                                    "                                                consult with the nearest Canadian embassy or consulate for the most\n" +
+                                                    "                                                up-to-date information."}
                                             </p>
                                         </div>
                                     </div>
@@ -250,29 +246,25 @@ export default function Faqs(): ReactElement {
                                             aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four"
                                         >
                                             <p className="text-gray-600">
-                                                As of my last update, the processing time for a Canadian Visitor Visa
-                                                for applicants from Colombia can vary significantly depending on a
-                                                number of factors such as the volume of applications received, the
-                                                completeness of the application, and individual circumstances of the
-                                                applicant.
-
-                                                Generally, the processing time can range from a few weeks to several
-                                                months. However, it's a good practice to check the most current
-                                                processing times on the official Immigration, Refugees and Citizenship
-                                                Canada (IRCC) website, as these times are updated regularly based on
-                                                actual processing conditions.
+                                                {" As of my last update, the processing time for a Canadian Visitor Visa\n" +
+                                                    "                                                for applicants from Colombia can vary significantly depending on a\n" +
+                                                    "                                                number of factors such as the volume of applications received, the\n" +
+                                                    "                                                completeness of the application, and individual circumstances of the\n" +
+                                                    "                                                applicant.\n" +
+                                                    "\n" +
+                                                    "                                                Generally, the processing time can range from a few weeks to several\n" +
+                                                    "                                                months. However, it's a good practice to check the most current\n" +
+                                                    "                                                processing times on the official Immigration, Refugees and Citizenship\n" +
+                                                    "                                                Canada (IRCC) website, as these times are updated regularly based on\n" +
+                                                    "                                                actual processing conditions."}
                                             </p>
                                         </div>
                                     </div>
                                 </div>
-                                {/* End Accordion */}
                             </div>
-                            {/* End Col */}
                         </div>
                     </div>
-                    {/* End Grid */}
                 </div>
-                {/* End FAQ */}
             </div>
         </div>
     );
